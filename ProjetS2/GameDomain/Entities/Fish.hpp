a90c699e2bf1d05f98fa363a366b088f3095043f
WSimard47536 <83228735+WSimard47536@users.noreply.github.com>
2024-02-19 01:44:18 -0500

    Setup steps

--- ProjetS2/GameDomain/Entities/Fish.hpp
+++ ProjetS2/GameDomain/Entities/Fish.hpp
@@ -1,8 +1,15 @@
 #pragma once
 
+#include <list>
+
 #include "Entity.hpp"
+#include "../Types/CaptureStep.hpp"
+
 
 class Fish : public Entity
 {
-
+private:
+  std::list<CaptureStep> captureSteps;
+  float length_m;
+  float weight_kg;
 };
---
+++ ProjetS2/GameDomain/Types/CaptureStep.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+typedef struct CaptureStep
+{
+  int speed_rotPerSec;
+  int margin;
+  int duration_s;
+};
